9d403b06c082fb822bcdc29d3984df9c0e314748
nyutorino <naoya3110823@yahoo.co.jp>
2016-06-20 17:36:12 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,6 +1,6 @@
 //答えを用意
 var number = Math.floor(Math.random()*3);
-var answer = parseInt(window.prompt)('数あてゲーム。0～2の数字を入力してください'));
+var answer = parseInt(window.prompt('数あてゲーム。0～2の数字を入力してください'));
 
 var message;
 if(answer === number){
